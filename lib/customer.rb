9df5e762c75d5478498fbf9ee049bce5b7c31644
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-08-14 04:45:50 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -8,7 +8,7 @@ class Customer
     @age = age
   end
 
-  def all
+  def self.all
     @@all
   end
 
--- lib/meal.rb
+++ lib/meal.rb
@@ -10,7 +10,7 @@ class Meal
     @tip = tip
   end
 
-  def all
+  def self.all
     @@all
   end
 
