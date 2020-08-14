af536a96924dc59029a4a16c92ce139c999c431d
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-08-14 05:37:16 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -21,5 +21,15 @@ class Customer
     Meal.new(waiter, self, total, tip)
   end
 
+  def meals
+    Meal.all.select do |meal|
+      meal.customer == self
+    end
+  end
+
+  def waiters
+    meals.map do |meal|
+      meal.waiter
+    end
 
 end
